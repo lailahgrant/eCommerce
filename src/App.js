f09168f2957841acb7e8383f401e56f9824baadf
lailahgrant <lailahgrant19@gmail.com>
2020-09-17 07:51:35 +0300

    routed Zoho.js

--- src/App.js
+++ src/App.js
@@ -12,6 +12,8 @@ import Default from './components/Default'
 
 import Modal from './components/Modal'  
 
+import Zoho from './components/Zoho'
+
 //package.json in the Cart folder is formed to make Cart.js as the main file
 //its done to avoid import Cart from './components/Cart/Cart'
 import Cart from './components/Cart'
@@ -26,7 +28,7 @@ import Cart from './components/Cart'
         <Route exact path="/" component={ProductList} />
         <Route path="/details" component={Details} />
         <Route path="/cart" component={Cart} />
-        <Route path="/zohoverify/verifyforzoho.html" component={} />
+        <Route path="/zohoverify/verifyforzoho.html" component={Zoho} />
         <Route  component={Default} />
       </Switch>
         
